baac16b78e68928073a2a7e14a7905d478c8bdd3
roleohibachi <michael.seery@afacademy.af.edu>
2023-04-24 16:48:51 -0600

    WIP: onedrive config generation

--- rclone2onedrive.py
+++ rclone2onedrive.py
@@ -1,52 +1,91 @@
 import json
-import configparser
+from configparser import ConfigParser
 import argparse
 import os
-print()
+import logging
+
+def get_rclone_details(remote_name: str, rclone_path: str = "~/.config/rclone/rclone.conf") -> tuple[str,str]:
+
+    #open file
+    rclone = ConfigParser()
+    rclone.read_file(open(os.path.expanduser(rclone_path)))
+
+    #sanity checks
+    if not remote_name in rclone:
+        logging.warn("An invalid rclone remote was requested. Here are the valid remotes in that file: ")
+        for section in rclone.sections:
+            if section["type"]=="onedrive":
+                logging.warn('\t'+str(section))
+        raise Exception
+
+    remote = rclone[remote_name]
+
+    if not remote["type"]=="onedrive":
+        logging.warn("An rclone remote was requested of some type other than Onedrive. ")
+        raise Exception
+    
+    #extract data
+
+    drive_id = remote["drive_id"]
+    refresh_token = str(json.loads(remote["token"])['refresh_token'])
+    
+    return drive_id, refresh_token
+
+def make_config_from_rclone_remote(remote_name: str, rclone_path: str = "~/.config/rclone/rclone.conf") -> tuple[ConfigParser,str]:
+    config = ConfigParser()
+    (drive_id, refresh_token) = get_rclone_details(remote_name=remote_name,rclone_path=rclone_path)
+    config['drive_id'] = drive_id
+    return config, refresh_token
+
+def print_shell_commands(remote_name: str, rclone_path: str = "~/.config/rclone/rclone.conf") -> None:
+    return
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--rcloneconfig', default=os.path.expanduser('~/.config/rclone/rclone.conf'), help='The rclone config file to draw from')
 parser.add_argument('--remote', help='The rclone args.remote to import')
 parser.add_argument('--syncpath', help='The directory to which to sync')
+parser.add_argument('--output', help='The desired output format. One of "bash" or "onedrive".')
 args = parser.parse_args()
 
 if not args.syncpath:
     args.syncpath = "~/"+args.remote
 
-config = configparser.ConfigParser()
-config.read_file(open(args.rcloneconfig))
+if(args.output == "bash"):
+    (drive_id,refresh_token) = get_rclone_details(args.remote, args.rcloneconfig)
 
-if not args.remote in config:
-    raise Exception
+    print("# make directories")
+    print("mkdir ~/.config/onedrive/" + args.remote)
+    print("mkdir ~/" + args.remote)
 
-refresh_token = str(json.loads(config[args.remote]["token"])['refresh_token'])
-drive_id = config[args.remote]["drive_id"]
+    print("")
+    print("# make config")
+    print("cat << EOF > ~/.config/onedrive/" + args.remote + "/config")
+    print("sync_dir = \"~/" + args.remote + "\"")
+    print("drive_id = \"" +drive_id + "\"")
+    print("EOF")
 
-print("# make directories")
-print("mkdir ~/.config/onedrive/" + args.remote)
-print("mkdir ~/" + args.remote)
+    print("")
+    print("# make refresh token")
+    print("ln -s ~/.config/onedrive/refresh_token ~/.config/onedrive/" + args.remote + "/refresh_token #reuses a \"parent\" onedrive token") 
+    print("cat << EOF > ~/.config/onedrive/" + args.remote + "/refresh_token" + "#reuses rclone's token")
+    print(refresh_token)
+    print("EOF")
 
-print("")
-print("# make config")
-print("cat << EOF > ~/.config/onedrive/" + args.remote + "/config")
-print("sync_dir = \"~/" + args.remote + "\"")
-print("drive_id = \"" +drive_id + "\"")
-print("EOF")
+    print("")
+    print("# test")
+    print("onedrive --confdir=~/.config/onedrive/" + args.remote + " --display-config")
 
-print("")
-print("# make refresh token")
-#print("ln -s ~/.config/onedrive/refresh_token ~/.config/onedrive/" + args.remote + "/refresh_token") #this reuses a "parent" token
-print("cat << EOF > ~/.config/onedrive/" + args.remote + "/refresh_token")
-print(refresh_token)
-print("EOF")
+    print("")
+    print("# system service")
+    print("sed 's#^\\(ExecStart.*\\)#\\1 --confdir=~/.config/onedrive/" + args.remote + "#' /usr/lib/systemd/user/onedrive.service | sudo tee /usr/lib/systemd/user/onedrive-" + args.remote + ".service")
+    print("systemctl --user enable onedrive-" + args.remote)
+    print("systemctl --user start onedrive-" + args.remote)
+    print("journalctl -f --user-unit=onedrive-" + args.remote)
+elif(args.output=="onedrive"):
+    (config, refresh_token) = make_config_from_rclone_remote(args.remote, args.rcloneconfig)
+    print("onedrive config: ")
+    print(config) #TODO can I do this
+    print("refresh_token:")
+    print(refresh_token)
 
-print("")
-print("# test")
-print("onedrive --confdir=~/.config/onedrive/" + args.remote + " --display-config")
-
-print("")
-print("# system service")
-print("sed 's#^\\(ExecStart.*\\)#\\1 --confdir=~/.config/onedrive/" + args.remote + "#' /usr/lib/systemd/user/onedrive.service | sudo tee /usr/lib/systemd/user/onedrive-" + args.remote + ".service")
-print("systemctl --user enable onedrive-" + args.remote)
-print("systemctl --user start onedrive-" + args.remote)
-print("journalctl -f --user-unit=onedrive-" + args.remote)
+exit(0)
